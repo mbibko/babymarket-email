0ea56aa61b292c0a933ee4e6023a3a8ad1fc5ac0
mbibko <mbibko@gmail.com>
2018-09-07 16:55:11 +0300

    Обновил код сборщика, изменил структуру проекта

--- app/dinamic-links.js
+++ app/dinamic-links.js
@@ -1,26 +1,47 @@
 ;
 (function() {
-    $.ajax({
-            'url': 'ajax.pages_list.php',
-            'type': 'get'
-        })
-        .fail(function() {})
-        .success(function(data) {
-            if (data.toString().indexOf('<?php') == 0) return;
-            var $wnd = $('<div style="color: #fff; position: absolute; top: 10px; left: 10px; background: #000; border: 2px solid #ccc; z-index: 101000; padding: 10px; "/>');
-            var $close = $('<a href="#" style="color: #fff; font-weight: normal; display: block; margin-bottom: 10px;">Список страниц :: Закрыть</a>')
-                .click(function(e) {
-                    e.preventDefault();
-                    $wnd.hide();
-                })
-                .appendTo($wnd);
-            var even = false;
-            $(data.pages).each(function() {
-                var page = this;
-                var style = even ? 'color: #000; background: #f5f4f4; ' : 'background: #fff; color: #000;'
-                $wnd.append('<a href="' + page + '" style="' + style + ' padding: 8px 10px; font-weight: normal; display: block;">' + page + '</a>');
-                even = !even;
-            });
-            $('body').append($wnd);
+    var request = new XMLHttpRequest();
+    request.open('GET', 'ajax.pages_list.php', true);
+
+    request.onload = function() {
+      if (this.status >= 200 && this.status < 400) {
+        // Success!
+        var resp = this.response;
+        console.log(resp);
+
+        if (resp.toString().indexOf('<?php') == 0) return;
+        var wnd = document.createElement("div");
+        wnd.style = 'color: #fff; position: absolute; top: 10px; left: 10px; background: #000; border: 2px solid #ccc; z-index: 101000; padding: 10px';
+        
+        var close = document.createElement("div");
+        close.style = 'color: #fff; font-weight: normal; display: block; margin-bottom: 10px; text-decoration: underline; cursor: pointer;';
+        close.innerHTML = 'Список страниц :: Закрыть';
+        close.addEventListener('click', function() {
+            wnd.style.display = 'none';
         });
+        wnd.appendChild(close);
+        
+        var even = false;
+        JSON.parse(resp).pages.forEach(function(item, i, arr) {
+            var style = even ? 'color: #000; background: #f5f4f4; ' : 'background: #fff; color: #000;';
+            var link = document.createElement("a");
+            link.style = style + ' padding: 8px 10px; font-weight: normal; display: block;';
+            link.href = item;
+            link.innerHTML = item;
+            wnd.appendChild(link);
+            even = !even;
+        });
+        
+        document.body.append(wnd);
+      } else {
+        // We reached our target server, but it returned an error
+
+      }
+    };
+
+    request.onerror = function() {
+      // There was a connection error of some sort
+    };
+
+    request.send();
 }());
