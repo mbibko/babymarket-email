94a08dc697ab504466070a5a22d15c1320b93e57
mbibko <mbibko@gmail.com>
2024-06-25 11:41:36 +0300

    refactor project

--- gulpfile.js
+++ gulpfile.js
@@ -1,132 +1,86 @@
-var gulp = require('gulp');
-var $ = require('gulp-load-plugins')();
-var browserSync = require('browser-sync').create();
-var nunjucksRender = require('gulp-nunjucks-render');
-var inlineCss = require('gulp-inline-css');
-var path = require('path');
-var inlineSource = require('gulp-inline-source');
+const gulp = require("gulp");
+const gulpSass = require("gulp-sass")(require("sass"));
+const browserSync = require("browser-sync").create();
+const nunjucksRender = require("gulp-nunjucks-render");
+const gulpInlineCss = require("gulp-inline-css");
+const sendmail = require("gulp-mailgun");
 
 function reload(cb) {
-  browserSync.reload()
-  cb()
+  browserSync.reload();
+  cb();
 }
 
-gulp.task('watch', function () {
-  gulp.watch([
-    'app/css/**/*.css',
-    '!app/css/main.css',
-    'app/js/**/*.js',
-    'app/images/**/*.{png,jpg,jpeg,gif,svg}',
-    'app/fonts/**/*.*'
-  ]).on('change', gulp.series(reload));
+function html() {
+  return gulp
+    .src("app/html/*.html")
+    .pipe(
+      nunjucksRender({
+        path: ["app/html/layouts"], // String or Array
+      })
+    )
+    .pipe(gulp.dest("build"));
+}
 
-  gulp.watch('app/scss/**/*.scss', gulp.series('sass'));
+function images() {
+  return gulp
+    .src("app/images/**/*.{png,jpg,jpeg,gif,svg}", { encoding: false })
+    .pipe(gulp.dest("build/images"));
+}
 
-  gulp.watch('app/scss/media.scss', gulp.series('media'));
+function sass() {
+  return gulp
+    .src("app/scss/main.scss")
+    .pipe(gulpSass())
+    .pipe(gulp.dest("build"))
+    .pipe(browserSync.stream());
+}
 
-  gulp.watch('app/html/**/*.html', gulp.series('html', reload));
-  gulp.watch('app/main.css', gulp.series('html', reload));
-  gulp.watch('app/css/media.css', gulp.series('html', reload));
-});
+function inlinecss() {
+  return gulp
+    .src("build/*.html")
+    .pipe(gulpInlineCss())
+    .pipe(gulp.dest("build"));
+}
 
-gulp.task('html', function () {
-  return gulp.src('app/html/*.html')
-    .pipe($.plumber({
-      errorHandler: $.notify.onError("<%= error.message %>")
-    }))
-    .pipe(nunjucksRender({
-      path: ['app/html/layouts'] // String or Array
-    }))
-    .pipe(inlineSource())
-    .pipe(inlineCss({
-      preserveMediaQueries: true
-    }))
-    .pipe($.replace('src="images/', 'src="http://html.xx28.ru/vsekroham-mail/images/'))
-    .pipe($.replace('url(images/', 'url(http://html.xx28.ru/vsekroham-mail/images/'))
-    // .pipe($.replace('<script src="https://code.jquery.com/jquery-1.9.1.min.js"></script>', ''))
-    // .pipe($.replace('<script src="dinamic-links.js"></script>', ''))
-    // .pipe($.htmlmin({ collapseWhitespace: true }))
-    .pipe(gulp.dest('build'));
-});
-
-gulp.task('sass', function () {
-  return gulp.src('app/scss/main.scss')
-    .pipe($.sourcemaps.init())
-    .pipe($.plumber({
-      errorHandler: $.notify.onError("<%= error.message %>")
-    }))
-    .pipe($.sass({
-      outputStyle: 'compressed', // libsass doesn't support expanded yet
-    }))
-    .pipe($.sourcemaps.write('./maps', {
-      includeContent: false,
-      sourceRoot: 'app/scss'
-    }))
-    .pipe(gulp.dest('build'))
-    .pipe(browserSync.stream({match: '**/*.css'}));
-});
-
-gulp.task('media', function () {
-  return gulp.src('app/scss/media.scss')
-    // .pipe($.sourcemaps.init())
-    .pipe($.plumber({
-      errorHandler: $.notify.onError("<%= error.message %>")
-    }))
-    .pipe($.sass({
-      outputStyle: 'compressed', // libsass doesn't support expanded yet
-    }))
-    .pipe(gulp.dest('app/css'))
-    .pipe(browserSync.stream({match: '**/*.css'}));
-});
-
-gulp.task('fonts', function () {
-  return gulp.src('bower_components/bootstrap-sass/assets/fonts/**/*')
-    .pipe(gulp.dest('app/fonts'));
-});
-
-gulp.task('serve', cb => {
+function serve(cb) {
   browserSync.init({
     notify: false,
     open: false,
     server: {
-      baseDir: ['build']
-    }
+      baseDir: ["build"],
+    },
   });
-  cb()
-});
-
-var options = {
-  user: 'api:key-c9370f6109ccb5fa6223fd4f673e36c4',
-  url: 'https://api.mailgun.net/v3/sandboxa7c0607c0d9d40abb5c50ca20018e3b1.mailgun.org/messages',
-  form: {
-    from: 'Maxim <postmaster@sandboxa7c0607c0d9d40abb5c50ca20018e3b1.mailgun.org>',
-    to: 'Maxim <mbibko@gmail.com>',
-    subject: 'You have an new email',
-    text: 'text version'
-  }
-};
-
-sendmail = require('gulp-mailgun');
-gulp.task('sendmail', cb => {
-  gulp.src(['build/payment-notification.html']) // Modify this to select the HTML file(s)
-    .pipe(sendmail({
-      key: 'key-c9370f6109ccb5fa6223fd4f673e36c4', // Enter your Mailgun API key here
-      sender: 'postmaster@sandboxa7c0607c0d9d40abb5c50ca20018e3b1.mailgun.org',
-      recipient: 'mbibko@gmail.com',
-      // recipient: 'alex@silentcode.org',
-      // recipient: 'frolovean@gmail.com',
-      subject: 'vsekroham mail'
-    }));
   cb();
+}
+
+function watch(cb) {
+  gulp.watch("app/images/**/*.*", gulp.series(images, reload));
+  gulp.watch("app/scss/**/*.scss", sass);
+  gulp.watch("app/html/**/*.html", gulp.series(html, reload));
+  cb();
+}
+
+function postBuildTask(cb) {
+  inlinecss();
+  cb();
+}
+
+gulp.task("sendmail", () => {
+  return gulp
+    .src(["build/payment-notification.html"]) // Modify this to select the HTML file(s)
+    .pipe(
+      sendmail({
+        key: "key-c9370f6109ccb5fa6223fd4f673e36c4", // Enter your Mailgun API key here
+        sender:
+          "postmaster@sandboxa7c0607c0d9d40abb5c50ca20018e3b1.mailgun.org",
+        recipient: "mbibko@gmail.com",
+        subject: "babymarket mail",
+      })
+    );
 });
 
-gulp.task('inlinecss', function () {
-  console.log('inlinecss start');
-  return gulp.src('app/*.html')
-    .pipe(inlineCss({
-      preserveMediaQueries: true
-    }))
-    .pipe(gulp.dest('app/'));
-});
-
-gulp.task('default', gulp.parallel('watch', 'serve'));
+gulp.task("default", gulp.parallel(watch, serve));
+gulp.task(
+  "build",
+  gulp.series(gulp.parallel(html, images, sass), postBuildTask)
+);
